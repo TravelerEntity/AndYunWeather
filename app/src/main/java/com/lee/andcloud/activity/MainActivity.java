c18f1e9f7b65cbfa828cd9b4b81d1c77fe16eec1
Yun Lin <lizhi.ace@qq.com>
2021-06-05 13:51:32 +0800

    加入了侧滑的DrawerLayout

--- app/src/main/java/com/lee/andcloud/activity/MainActivity.java
+++ app/src/main/java/com/lee/andcloud/activity/MainActivity.java
@@ -1,25 +1,79 @@
 package com.lee.andcloud.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.lee.andcloud.R;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar tbHead;
+    private Toolbar tbHead;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWidget();
-
+        /*用toolbar替代actionbar*/
         setSupportActionBar(tbHead);
+        loadNavBtn();
     }
 
+    private void loadNavBtn() {
+        /*获取actionbar实例，不为空才会进行后面的设置*/
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_location_city_24);
+        }
+    }
+
+    /**
+     * 加载toolbar上的选项时系统调用
+     * @param menu 要加载到这个menu上
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /*加载菜单布局*/
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    /**
+     * toolbar上按钮被点击时调用
+     * @param item 被点击的按钮
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId() ){
+            case R.id.it_more:
+                Toast.makeText(this, "点击了更多按钮", Toast.LENGTH_SHORT).show();
+                break;
+            /*toolbar上的导航按钮被点击时，最左边按钮*/
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            default:
+        }
+        return true;
+    }
+
+    /**
+     * 初始化界面的View
+     */
     private void initWidget() {
         tbHead = findViewById(R.id.tb_head);
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
 }
